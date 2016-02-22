8ae1bcf92c63f0d747dced44cb95f1e7eb1fee99
Eric Moyer <eric_moyer@yahoo.com>
2016-02-21 21:26:37 -0500

    Include world header in main file and instantiate

--- ALAndroid.cpp
+++ ALAndroid.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "world.hpp"
 using namespace std;
 
 int main(int, char**){
-	cout << "Stuff" << endl;
-	return 0;
+    World w;
+    cout << "Stuff" << endl;
+    return 0;
 }
