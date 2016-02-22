0562f5e5d313f39a7b99ac1bb9865624ff748963
Eric Moyer <eric_moyer@yahoo.com>
2016-02-21 21:27:31 -0500

    Sketch out a little more of the Rule interface

--- world.hpp
+++ world.hpp
@@ -1,8 +1,34 @@
 #include <string>
 #include <vector>
 
-using Chem = int;
 using Offset = int;
+using Chem = unsigned;
+
+/// An unsigned integer weight - must be greater than 0
+class Weight {
+    unsigned w;
+public:
+    /// Initialize the weight from an unsigned integer. 0 becomes 1.
+    Weight(unsigned w):w{w==0?1:w} {}
+    operator unsigned(){ return w; }
+};
+
+class WeightedChemList {
+    template<typename T>
+    using vector=std::vector<T>;
+
+    vector<Chem> chems;
+    vector<Weight> weights;
+    unsigned weight_sum;
+public:
+    WeightedChemList(): chems{}, weights{}, weight_sum{0} {}
+
+    void push_back(Chem c, Weight w){
+	chems.push_back(c);
+	weights.push_back(w);
+	weight_sum += w;
+    }
+};
 
 class Rule {
 	template<typename T>
@@ -10,13 +36,14 @@ class Rule {
 
     vector<Offset> offsets;
     vector<Chem> inputs;
-    vector<int> weights;
-    int weight_sum;
-    vector<vector<Chem>> outputs;
-
-    /// Return true if the rule was successfully applied at \a index
+    unsigned weight_sum;
+    vector<unsigned> weights;
+    vector<vector<Chem> > outputs;
+public:
+    /// Apply the rule was successfully applied at \a index
     template<typename T>
-    bool apply(vector<Chem>& , int index) 
+    void apply(vector<Chem>& in, vector<WeightedChemList>& out, int index){
+    }
 };
 
 
@@ -24,5 +51,5 @@ struct World {
 	template<typename T>
 	using vector=std::vector<T>;
 
-    vector<Rule> rules;	
+    vector<Rule> rules;
 };
